a70215430c8d2c0e40ded52bb4f0a747c523bbcf
Tales-MR <121312393+Tales-MR@users.noreply.github.com>
2023-01-04 13:30:41 -0300

    mais testes JS

--- Exercícios/PastaPai/PráticaSolo/site01/interatividade.js
+++ Exercícios/PastaPai/PráticaSolo/site01/interatividade.js
@@ -6,12 +6,10 @@ var menu = document.querySelector('nav#menu')
 var tela = document.querySelector('body')
 
 //Detectando redimensionamento
-tela.addEventListener('online', controle)
+tamanho = tela.clientWidth
 
-function controle() {
-    tamanho = tela.clientWidth
-
-    if (tamanho < 630) {
-        menu.style.display = 'none';
-    }
+if (tamanho < 630) {
+    menu.style.display = 'none';
 }
+
+$(document) 
